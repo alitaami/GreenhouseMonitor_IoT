b76adf87e75e5f8fb5c2500dba3abd508dac4674
ali <alitaami81@gmail.com>
2024-06-03 13:50:00 +0330

    Added SensorData model

---
+++ Iot_WebApp/Models/SensorData.cs
@@ -0,0 +1,10 @@
+﻿namespace Iot_WebApp.Models
+{
+    public class SensorData
+    {
+        public int Id { get; set; }
+        public float Temperature { get; set; }
+        public float Humidity { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.Now;
+    }
+}
