e46f20afce2d98f88fef497f2ce0790882124304
ali <alitaami81@gmail.com>
2024-06-03 14:11:42 +0330

    Added SensorDataController

---
+++ Iot_WebApp/Controllers/SensorDataController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore; 
+using System.Threading.Tasks;
+using Iot_WebApp.Data;
+using Iot_WebApp.Models;
+
+namespace IoTWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SensorDataController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SensorDataController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostSensorData([FromBody] SensorData sensorData)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.SensorData.Add(sensorData);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetSensorData()
+        {
+            var data = await _context.SensorData.ToListAsync();
+            return Ok(data);
+        }
+    }
+}
